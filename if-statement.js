4316b76a40255ff03e2273cd56f224096bc569b4
vibguldev <gulativibhav@gmail.com>
2017-01-19 14:30:31 +0530

    added if-statement.js

---
+++ if-statement.js
@@ -0,0 +1,16 @@
+var fruitName = 'orange';
+
+fruitNameLength = fruitName.length;
+
+if (fruitNameLength > 5)
+{
+    console.log('The fruit name has more than five characters.');
+}
+else if (fruitNameLength < 5 && fruitNameLength >0)
+{
+    console.log('The fruit name has five characters or less.');
+}
+else
+{
+    console.log('Fruit name not added')
+}
