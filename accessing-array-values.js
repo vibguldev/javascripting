3a33947e501c755e0e31e11a12cb55c5700b238b
vibguldev <gulativibhav@gmail.com>
2017-01-19 15:00:42 +0530

    added accessing-array-values.js

---
+++ accessing-array-values.js
@@ -0,0 +1,2 @@
+var foodItems = ['apple', 'pizza', 'pear'];
+console.log(foodItems[1]);
--- array-filtering.js
+++ array-filtering.js
@@ -1,6 +1,6 @@
-var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-var filtered = numbers.filter(function evenNumbers (number) {
+var listOfNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var filteredNumbers = listOfNumbers.filter(function evenNumbers (number) {
        return number % 2 === 0;
      }) 
 
-console.log(filtered);
+console.log(filteredNumbers);
